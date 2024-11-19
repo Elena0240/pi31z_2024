1564ee10c39a66f2f31bdd67d568dc23f811f202
Элена Баязитова <elenkaaaa734@gmail.com>
2024-11-19 09:06:49 +0500

    добавление

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php 
-require_once ('src/Pawn.php');
-require_once
+<?php
+
+require_once ('src/color.php');
+require_once ('src/Board.php');
+
+$board = new Board();
+$board->printBoard();
--- src/figure.php
+++ src/figure.php
@@ -1,5 +1,28 @@
-<?php 
+<?php
 require_once('./color.php');
-public function __construct (Color $color);
-public function getColor(): Color ;
-public function getIcon(): string;
+
+
+interface IFigure {
+    public function __construct(Color $color);
+    public function getColor(): Color;
+    public function getIcon(): string;
+}
+
+abstract class Figure implements IFigure {
+    private Color $color;
+    protected string $icon;
+
+    public function __construct(Color $color){
+        $this->color = $color;
+    }
+
+    public function getColor(): Color {
+        return $this->color;
+    }
+
+    public function getIcon(): string {
+        $prefix = $this->color === Color::Black ? 'b' : 'w';
+        return $prefix . $this->icon;
+    }
+
+}
