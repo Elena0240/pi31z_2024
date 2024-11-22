3d612e0c3274cc48959ab038267f6b6842647a0b
Элена Баязитова <elenkaaaa734@gmail.com>
2024-11-22 08:54:46 +0500

    Добавлен King

--- src/King.php
+++ src/King.php
@@ -5,4 +5,17 @@ require_once('IFigure.php');
 class King extends Figure {
     protected array $icon = ["\u{265A}", "\u{2654}"];
 
+    public function canMove(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
+        $row_diff = abs($to_row - $from_row);
+        $col_diff = abs($to_col - $from_col);
+
+        if ($row_diff <= 1 && $col_diff <= 1) {
+            $target_item = $board->getItem($to_row, $to_col);
+            if ($target_item === null || $target_item->color !== $this->color) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
